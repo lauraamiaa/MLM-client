1b55866729c5fe6b360f89c0e29625961491d41c
marijn-v <marijnverbiesen@gmail.com>
2021-10-21 15:52:12 +0200

    working on display language button

--- src/components/Buttons/LanguageButton.js
+++ src/components/Buttons/LanguageButton.js
@@ -2,7 +2,10 @@ import React from "react";
 
 import "./LanguageButton.css";
 
-export default function LanguageButton() {
+export default function LanguageButton(props) {
+  const { response } = props;
+  console.log("props", response);
+
   return (
     <div>
       <a href="#" class="button">
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import "./Homepage.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-// import LanguageButton from "../Buttons/LanguageButton";
+import { fetchResponses } from "../../store/responses/actions";
+import { selectResponses } from "../../store/responses/selector";
+import LanguageButton from "../Buttons/LanguageButton";
 
 export default function Homepage() {
+  const dispatch = useDispatch();
+  const responses = useSelector(selectResponses);
+  console.log("selector", responses);
+
+  useEffect(() => {
+    dispatch(fetchResponses());
+  }, []);
+
   return (
     <div>
       <Header />
+      {responses.map((response) => {
+        return <LanguageButton key={response.id} responses={response} />; // pass down as props
+      })}{" "}
       {/* <LanguageButton /> */}
       <div className="homepageBody">
         <div className="sceneContainer"></div>
