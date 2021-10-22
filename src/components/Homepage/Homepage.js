8de21a0f426108e07301677437f289c98b85990f
marijn-v <marijnverbiesen@gmail.com>
2021-10-22 14:24:37 +0200

    added audio to languagebuttons

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -6,9 +6,20 @@ import Footer from "../Footer/Footer";
 import LanguageButtons from "../Buttons/LanguageButtons";
 import { selectChoice } from "../../store/responses/selector";
 import Scene from "../Scene/Scene";
+import { useEffect } from "react";
 
 export default function Homepage() {
   const choice = useSelector(selectChoice);
+
+  useEffect(() => {
+    if (choice) {
+      console.log("choiceChange", choice.audio);
+      // console.log(audioElement.current);
+      const audioElement = new Audio(choice.audio);
+      audioElement.play();
+    }
+  }, [choice]);
+
   return (
     <div>
       <Header />
