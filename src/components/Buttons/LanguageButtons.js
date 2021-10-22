3799647f3d05d00fd36e5ea001b74e65ed3c2b2b
marijn-v <marijnverbiesen@gmail.com>
2021-10-22 15:03:18 +0200

    move languagebutton logic to own component

--- src/components/Buttons/LanguageButtons.js
+++ src/components/Buttons/LanguageButtons.js
@@ -5,6 +5,7 @@ import "./LanguageButton.css";
 
 import { fetchResponses, responseChosen } from "../../store/responses/actions";
 import { selectResponses } from "../../store/responses/selector";
+import { selectChoice } from "../../store/responses/selector";
 
 export default function LanguageButton() {
   const dispatch = useDispatch();
@@ -27,6 +28,17 @@ export default function LanguageButton() {
     );
   }
 
+  const choice = useSelector(selectChoice);
+
+  useEffect(() => {
+    if (choice) {
+      console.log("choiceChange", choice.audio);
+      // console.log(audioElement.current);
+      const audioElement = new Audio(choice.audio);
+      audioElement.play();
+    }
+  }, [choice]);
+
   return (
     <div>
       {responses &&
@@ -41,6 +53,7 @@ export default function LanguageButton() {
             </div>
           );
         })}
+      <div> {choice && <p>{choice.expression}</p>}</div>
     </div>
   );
 }
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -7,16 +7,16 @@ import Scene from "../Scene/Scene";
 import { useEffect } from "react";
 
 export default function Homepage() {
-  const choice = useSelector(selectChoice);
+  // const choice = useSelector(selectChoice);
 
-  useEffect(() => {
-    if (choice) {
-      console.log("choiceChange", choice.audio);
-      // console.log(audioElement.current);
-      const audioElement = new Audio(choice.audio);
-      audioElement.play();
-    }
-  }, [choice]);
+  // useEffect(() => {
+  //   if (choice) {
+  //     console.log("choiceChange", choice.audio);
+  //     // console.log(audioElement.current);
+  //     const audioElement = new Audio(choice.audio);
+  //     audioElement.play();
+  //   }
+  // }, [choice]);
 
   return (
     <div>
