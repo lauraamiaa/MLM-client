05c6541a96e18ffe73a0c0988ccda3b028947644
marijn-v <marijnverbiesen@gmail.com>
2021-10-22 15:05:11 +0200

    clean up

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,23 +1,11 @@
 import React from "react";
-
 import "./Homepage.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import LanguageButtons from "../Buttons/LanguageButtons";
 import Scene from "../Scene/Scene";
-import { useEffect } from "react";
 
 export default function Homepage() {
-  // const choice = useSelector(selectChoice);
-
-  // useEffect(() => {
-  //   if (choice) {
-  //     console.log("choiceChange", choice.audio);
-  //     // console.log(audioElement.current);
-  //     const audioElement = new Audio(choice.audio);
-  //     audioElement.play();
-  //   }
-  // }, [choice]);
-
   return (
     <div>
       <Header />
