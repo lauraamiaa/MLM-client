4592e5ec7430b0bc8fc2c2f424c47c3612fbbc1d
marulita18 <crespo.mariel@gmail.com>
2021-10-25 15:33:58 +0200

    added admin page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Homepage from "./components/Homepage/Homepage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/admin" component={Dashboard} />
+        <Route exact path="/" component={Homepage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
