aa1b0397f6f964468c9c43ce9f679966316b107b
lauraamiaa <miatkowski.laura@gmail.com>
2021-10-26 14:18:46 +0200

    adjusted heading

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="titleContainer">
-      <h1 className="siteTitle">BARK BACK</h1>
+      <h1 className="siteTitle">BARK BACK !</h1>
     </div>
   );
 }
